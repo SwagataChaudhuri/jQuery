661b4d260bb943f22641e87d5b2d53cdbe571a3a
Swagata Chaudhuri <swagatachaudhuri@outlook.com>
2017-12-04 23:36:11 +0530

    Code Check In

---
+++ custom.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    setTimeout(
+        function () {
+            $("#testMessage").slideUp("slow");
+        }, 5000
+    );
+    $("<p><span>If you click on me, I will disappear</span></p>").appendTo(".mainDiv");
+    $("<p><span>Click on me!!!</span></p>").appendTo(".mainDiv");
+    $("<p><span>Click on me too!!!</span></p>").appendTo(".mainDiv");
+    $('<p/>', {
+        id: 'message1',
+        class: 'mytest',
+        text: 'Click me once to disappear with an alert'
+    }).appendTo('.mainDiv');
+    $('<p/>', {
+        id: 'message2',
+        class: 'mytest',
+        text: 'Click me twice to disappear with an alert'
+    }).appendTo('.mainDiv');
+    $("span").css("color", "lightgreen");
+    $("span").css("cursor", "pointer");
+    $("p").click(function () {
+        $(this).slideUp("slow");
+    });
+    $("#message1").on("click", function () {
+        alert('I am clicked one time!!!');
+    });
+    $("#message2").dblclick(function () {
+        alert('I am double clicked!!!');
+    });
+});
